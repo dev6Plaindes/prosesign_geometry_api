de2f946d92b5f559f9503045004f72ac48d6f4b9
dev6Plaindes <desarrollador6@plaindes.com.pe>
2026-03-31 11:13:09 -0500

    config .env

--- src/auto_plano/repository.py
+++ src/auto_plano/repository.py
@@ -3,11 +3,11 @@ import os
 from sqlalchemy import create_engine, text
 
 # Configuramos la URL de conexión usando tus variables
-DB_USER = "root"
-DB_PASS = "rootpassword"
-DB_HOST = "localhost"
-DB_PORT = "3306"
-DB_NAME = "db_arquitectura"
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASS = os.getenv("DB_PASS", "rootpassword")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "db_arquitectura")
 
 # Cadena de conexión para MySQL
 DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
